433439f9b3c532b6e6d36caee7cd01b3a58d13c2
AlexisQuintero <alexis_quintero@hotmail.com.ar>
2015-08-15 00:28:13 -0300

    Mejorando capa presentacion

---
+++ src/negocio/ControladorAjedrez.java
@@ -0,0 +1,15 @@
+package negocio;
+
+public class ControladorAjedrez {
+	
+	public void movimiento(String desde, String hasta){
+		
+		System.out.println("desde: " + desde + " hasta: " + hasta);
+		
+	}
+	
+	public void actualizarTablero(int estado){
+		
+	}
+
+}
--- src/presentacion/VentanaPrincipal.java
+++ src/presentacion/VentanaPrincipal.java
@@ -2,14 +2,30 @@ package presentacion;
 
 import java.awt.EventQueue;
 
+
+
 import javax.swing.JFrame;
+
+import negocio.ControladorAjedrez;
 import net.miginfocom.swing.MigLayout;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
 import java.awt.GridLayout;
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaPrincipal {
+	
+	private int estado = 0; //Indica el estado del sistema respecto a los clicks, ninguno == 0, 1ro == 1, 2do == 2
+							//El primer click indica la posición de inicio, el segundo click indica la posición de destino
+	
+	private ControladorAjedrez ca = new ControladorAjedrez();
+	
+	private StringBuilder desde = new StringBuilder();
+	private StringBuilder hasta = new StringBuilder();
 
 	private JFrame frame;
 
@@ -50,105 +66,170 @@ public class VentanaPrincipal {
 		panel.setLayout(new GridLayout(0, 8, 0, 0));
 		
 		JButton btnA8 = new JButton("a8");
+		btnA8.setActionCommand("A8");
+		btnA8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnA8.setForeground(new Color(255, 255, 255));
 		btnA8.setBackground(new Color(204, 153, 51));
 		panel.add(btnA8);
 		
 		JButton btnB8 = new JButton("b8");
+		btnB8.setActionCommand("B8");
+		btnB8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnB8.setForeground(new Color(255, 255, 255));
 		btnB8.setBackground(new Color(102, 51, 0));
 		panel.add(btnB8);
 		
 		JButton btnC8 = new JButton("c8");
+		btnC8.setActionCommand("C8");
+		btnC8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnC8.setForeground(new Color(255, 255, 255));
 		btnC8.setBackground(new Color(204, 153, 51));
 		panel.add(btnC8);
 		
 		JButton btnD8 = new JButton("d8");
+		btnD8.setActionCommand("D8");
 		btnD8.setForeground(new Color(255, 255, 255));
 		btnD8.setBackground(new Color(102, 51, 0));
 		panel.add(btnD8);
 		
 		JButton btnE8 = new JButton("e8");
+		btnE8.setActionCommand("E8");
 		btnE8.setForeground(new Color(255, 255, 255));
 		btnE8.setBackground(new Color(204, 153, 51));
 		panel.add(btnE8);
 		
 		JButton btnF8 = new JButton("f8");
+		btnF8.setActionCommand("F8");
 		btnF8.setForeground(new Color(255, 255, 255));
 		btnF8.setBackground(new Color(102, 51, 0));
 		panel.add(btnF8);
 		
 		JButton btnG8 = new JButton("g8");
+		btnG8.setActionCommand("G8");
 		btnG8.setForeground(new Color(255, 255, 255));
 		btnG8.setBackground(new Color(204, 153, 51));
 		panel.add(btnG8);
 		
 		JButton btnH8 = new JButton("h8");
+		btnH8.setActionCommand("H8");
 		btnH8.setForeground(new Color(255, 255, 255));
 		btnH8.setBackground(new Color(102, 51, 0));
 		panel.add(btnH8);
 		
 		JButton btnA7 = new JButton("a7");
+		btnA7.setActionCommand("A7");
+		btnA7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnA7.setForeground(new Color(255, 255, 255));
 		btnA7.setBackground(new Color(102, 51, 0));
 		panel.add(btnA7);
 		
 		JButton btnB7 = new JButton("b7");
+		btnB7.setActionCommand("B7");
+		btnB7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnB7.setForeground(new Color(255, 255, 255));
 		btnB7.setBackground(new Color(204, 153, 51));
 		panel.add(btnB7);
 		
 		JButton btnC7 = new JButton("c7");
+		btnC7.setActionCommand("C7");
+		btnC7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnC7.setForeground(new Color(255, 255, 255));
 		btnC7.setBackground(new Color(102, 51, 0));
 		panel.add(btnC7);
 		
 		JButton btnD7 = new JButton("d7");
+		btnD7.setActionCommand("D7");
 		btnD7.setForeground(new Color(255, 255, 255));
 		btnD7.setBackground(new Color(204, 153, 51));
 		panel.add(btnD7);
 		
 		JButton btnE7 = new JButton("e7");
+		btnE7.setActionCommand("E7");
 		btnE7.setForeground(new Color(255, 255, 255));
 		btnE7.setBackground(new Color(102, 51, 0));
 		panel.add(btnE7);
 		
 		JButton btnF7 = new JButton("f7");
+		btnF7.setActionCommand("F7");
 		btnF7.setForeground(new Color(255, 255, 255));
 		btnF7.setBackground(new Color(204, 153, 51));
 		panel.add(btnF7);
 		
 		JButton btnG7 = new JButton("g7");
+		btnG7.setActionCommand("G7");
 		btnG7.setForeground(new Color(255, 255, 255));
 		btnG7.setBackground(new Color(102, 51, 0));
 		panel.add(btnG7);
 		
 		JButton btnH7 = new JButton("h7");
+		btnH7.setActionCommand("H7");
 		btnH7.setForeground(new Color(255, 255, 255));
 		btnH7.setBackground(new Color(204, 153, 51));
 		panel.add(btnH7);
 		
 		JButton btnA6 = new JButton("a6");
+		btnA6.setActionCommand("A6");
+		btnA6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnA6.setForeground(new Color(255, 255, 255));
 		btnA6.setBackground(new Color(204, 153, 51));
 		panel.add(btnA6);
 		
 		JButton btnB6 = new JButton("b6");
+		btnB6.setActionCommand("B6");
+		btnB6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnB6.setForeground(new Color(255, 255, 255));
 		btnB6.setBackground(new Color(102, 51, 0));
 		panel.add(btnB6);
 		
 		JButton btnC6 = new JButton("c6");
+		btnC6.setActionCommand("C6");
+		btnC6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnC6.setForeground(new Color(255, 255, 255));
 		btnC6.setBackground(new Color(204, 153, 51));
 		panel.add(btnC6);
 		
 		JButton btnD6 = new JButton("d6");
+		btnD6.setActionCommand("D6");
 		btnD6.setForeground(new Color(255, 255, 255));
 		btnD6.setBackground(new Color(102, 51, 0));
 		panel.add(btnD6);
-		
+//TODO: segui con btn.setActionCommand()		
 		JButton btnE6 = new JButton("e6");
 		btnE6.setForeground(new Color(255, 255, 255));
 		btnE6.setBackground(new Color(204, 153, 51));
@@ -170,16 +251,31 @@ public class VentanaPrincipal {
 		panel.add(btnH6);
 		
 		JButton btnA5 = new JButton("a5");
+		btnA5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnA5.setForeground(new Color(255, 255, 255));
 		btnA5.setBackground(new Color(102, 51, 0));
 		panel.add(btnA5);
 		
 		JButton btnB5 = new JButton("b5");
+		btnB5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnB5.setForeground(new Color(255, 255, 255));
 		btnB5.setBackground(new Color(204, 153, 51));
 		panel.add(btnB5);
 		
 		JButton btnC5 = new JButton("c5");
+		btnC5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnC5.setForeground(new Color(255, 255, 255));
 		btnC5.setBackground(new Color(102, 51, 0));
 		panel.add(btnC5);
@@ -210,16 +306,31 @@ public class VentanaPrincipal {
 		panel.add(btnH5);
 		
 		JButton btnA4 = new JButton("a4");
+		btnA4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnA4.setForeground(new Color(255, 255, 255));
 		btnA4.setBackground(new Color(204, 153, 51));
 		panel.add(btnA4);
 		
 		JButton btnB4 = new JButton("b4");
+		btnB4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnB4.setForeground(new Color(255, 255, 255));
 		btnB4.setBackground(new Color(102, 51, 0));
 		panel.add(btnB4);
 		
 		JButton btnC4 = new JButton("c4");
+		btnC4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnC4.setForeground(new Color(255, 255, 255));
 		btnC4.setBackground(new Color(204, 153, 51));
 		panel.add(btnC4);
@@ -250,16 +361,31 @@ public class VentanaPrincipal {
 		panel.add(btnH4);
 		
 		JButton btnA3 = new JButton("a3");
+		btnA3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnA3.setForeground(new Color(255, 255, 255));
 		btnA3.setBackground(new Color(102, 51, 0));
 		panel.add(btnA3);
 		
 		JButton btnB3 = new JButton("b3");
+		btnB3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnB3.setForeground(new Color(255, 255, 255));
 		btnB3.setBackground(new Color(204, 153, 51));
 		panel.add(btnB3);
 		
 		JButton btnC3 = new JButton("c3");
+		btnC3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnC3.setForeground(new Color(255, 255, 255));
 		btnC3.setBackground(new Color(102, 51, 0));
 		panel.add(btnC3);
@@ -290,16 +416,31 @@ public class VentanaPrincipal {
 		panel.add(btnH3);
 		
 		JButton btnA2 = new JButton("a2");
+		btnA2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {			
+				click(e);			
+			}
+		});
 		btnA2.setForeground(new Color(255, 255, 255));
 		btnA2.setBackground(new Color(204, 153, 51));
 		panel.add(btnA2);
 		
 		JButton btnB2 = new JButton("b2");
+		btnB2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnB2.setForeground(new Color(255, 255, 255));
 		btnB2.setBackground(new Color(102, 51, 0));
 		panel.add(btnB2);
 		
 		JButton btnC2 = new JButton("c2");
+		btnC2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnC2.setForeground(new Color(255, 255, 255));
 		btnC2.setBackground(new Color(204, 153, 51));
 		panel.add(btnC2);
@@ -330,21 +471,43 @@ public class VentanaPrincipal {
 		panel.add(btnH2);
 		
 		JButton btnA1 = new JButton("a1");
+		btnA1.setActionCommand("A1");
+		btnA1.setText("asd");
+		btnA1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {				
+				click(e);			
+			}
+		});
 		btnA1.setBackground(new Color(102, 51, 0));
 		btnA1.setForeground(new Color(255, 255, 255));
 		panel.add(btnA1);
 		
 		JButton btnB1 = new JButton("b1");
+		btnB1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnB1.setForeground(new Color(255, 255, 255));
 		btnB1.setBackground(new Color(204, 153, 51));
 		panel.add(btnB1);
 		
 		JButton btnC1 = new JButton("c1");
+		btnC1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnC1.setForeground(new Color(255, 255, 255));
 		btnC1.setBackground(new Color(102, 51, 0));
 		panel.add(btnC1);
 		
 		JButton btnD1 = new JButton("d1");
+		btnD1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
 		btnD1.setForeground(new Color(255, 255, 255));
 		btnD1.setBackground(new Color(204, 153, 51));
 		panel.add(btnD1);
@@ -383,6 +546,33 @@ public class VentanaPrincipal {
 		
 		JPanel panelJugador1 = new JPanel();
 		frame.getContentPane().add(panelJugador1, "cell 1 4,grow");
+	}
+	
+	private void click(ActionEvent e){
+		
+		estado = estado == 0 ? 1 : estado;
+		
+		switch (estado) {
+		
+		case 1:
+			desde.append(e.getActionCommand());
+			estado = 2;
+			break;
+			
+		case 2:
+			hasta.append(e.getActionCommand());		
+			ca.movimiento(desde.toString(), hasta.toString());
+			estado = 0;
+			hasta.delete(0, 2);
+			desde.delete(0, 2);
+			break;
+
+		default:
+			break;
+		}
+		
+		ca.actualizarTablero(estado);
+		
 	}
 
 }
